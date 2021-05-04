03334d09a7956848f55e44e9604533a410aa4d84
thinh.ho <thinh.ho@logigear.com>
2021-05-04 13:36:27 +0700

    update TC01

--- src/main/java/helpers/Constant.java
+++ src/main/java/helpers/Constant.java
@@ -1,6 +1,11 @@
 package helpers;
 
 public class Constant {
+    public static final String RAILWAY_URL = "http://www.railway2.somee.com/";
+
+    public static final String EMAIL = "thinhho@gmail.com";
+    public static final String PASSWORD = "123123123";
+
     public static final Integer TIME_OUT = 10;
     public static final Integer IMPLICITLY_TIME = 20;
     public static final Integer SHORT_TIME = 3;
