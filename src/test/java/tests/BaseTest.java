2d1cf564096e0772f947935198129315886eb422
thinh.ho <thinh.ho@logigear.com>
2021-05-05 09:25:38 +0700

    update testNG

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,12 +5,17 @@ import helpers.Constant;
 import helpers.LogHelper;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.util.Locale;
 
 public class BaseTest {
     @BeforeMethod
-    public void beforeMethod() {
+    @Parameters("browser")
+    public void beforeMethod(@Optional("chrome") String browser) {
         LogHelper.info("Pre-condition");
-        BrowserHelper.startBrowser(BrowserHelper.DriverType.CHROME);
+        BrowserHelper.startBrowser(BrowserHelper.DriverType.valueOf(browser.toUpperCase()));
         BrowserHelper.navigateToUrl(Constant.RAILWAY_URL);
     }
 
